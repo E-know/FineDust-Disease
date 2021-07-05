e88c00caabaf8f0ac93efeb4e526b843cf9ad653
Inho Choi <eknow25@gmail.com>
2021-07-05 11:21:01 +0900

    Fix Error

--- src/health/Add_MSS_To_Data.py
+++ src/health/Add_MSS_To_Data.py
@@ -79,7 +79,6 @@ def convert_date_TO_dust(result, year):
 			if not pd.isnull(result.at[PERSON_ID, alp + '_DATE']):
 				sido = str(result.at[PERSON_ID, 'SIDO'])
 				sgg = str(result.at[PERSON_ID, 'SGG'])
-				result.at[PERSON_ID, alp + '_DATE'] = dust.at[sido + '_' + sgg, result.at[PERSON_ID, alp + '_DATE']]
-
+				result.at[PERSON_ID, alp + '_DATE'] = dust.at[sido + '_' + sgg, str(result.at[PERSON_ID, alp + '_DATE'])]
 
 main()
