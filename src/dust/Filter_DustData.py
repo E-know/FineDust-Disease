2e55cb21273934cd66419b706e2623bd0502219f
Inho Choi <eknow25@gmail.com>
2021-07-09 13:20:27 +0900

    에러 수정

--- src/dust/Filter_DustData.py
+++ src/dust/Filter_DustData.py
@@ -1,19 +1,29 @@
 import pandas as pd
 import numpy as np
-import googlemaps
 import os
-# Plz input your Google geocode API
-# 사용자의 구글 지오코드 API 키 값이 환경변수로 저장되어 있어야합니다.
 
-start_year = 2002 # Plz Input Start Year
-end_year = 2013 # Plz Input End Year
+start_year = 2002  # Plz Input Start Year
+end_year = 2002  # Plz Input End Year
 
-geo_api = os.environ['GEOCODE_API']
-map = googlemaps.Client(key=geo_api)
+
+def get_sido_sgg_code():
+	answer = {}
+	
+	sido_sgg = pd.read_csv('../../data/raw_dust/sido_sgg_code.csv')
+	
+	for index in sido_sgg.index:
+		row = sido_sgg.loc[index]
+		sido = str(row.at['SIDO_CODE'])
+		sgg = str(row.at['SGG_CODE'])
+		lat = row.at['lat']
+		lon = row.at['lon']
+		dict = {'lat': lat, 'lon': lon}
+		answer.update({sido + '_' + sgg: dict})
+	return answer
 
 
 def get_processed_data(year, quat):
-	data_name = './../../data/dust/' + str(year) + '년0' + str(quat) + '분기.xlsx'
+	data_name = './../../data/raw_dust/' + str(year) + '년0' + str(quat) + '분기.xlsx'
 	data_excel = pd.read_excel(data_name)
 	# data_excel = 측정소에서 얻어온 결과값 엑셀
 	
@@ -23,7 +33,7 @@ def get_processed_data(year, quat):
 	# 필요 없는 데이터값 제거
 	
 	result_excel = pd.DataFrame(columns=['lat', 'lon'])
-	ms_code = None # Measure Station Code
+	ms_code = None  # Measure Station Code
 	date = None
 	sum_pm = 0
 	count = 0
@@ -33,19 +43,19 @@ def get_processed_data(year, quat):
 		
 		# 측정일시가 달라지면 새로운 열 생
 		if date != str(row.at['측정일시'])[:8]:
-			input_dust_day_data(result_excel, ms_code, sum_pm, count)
+			input_dust_day_data(result_excel, ms_code, date, sum_pm, count)
 			sum_pm = 0
 			count = 0
-
+			
 			date = str(row['측정일시'])[:8]
-			
-			
+		
 		# 측정소코드(측정소 장소)가 달라지면 새로운 열 생성
 		if ms_code != row.at['측정소코드']:
-			input_dust_day_data(result_excel, ms_code, sum_pm, count)
+			input_dust_day_data(result_excel, ms_code, date, sum_pm, count)
 			sum_pm = 0
 			count = 0
-			
+			# TODO GEOCODE 사용하지 않는 방향으로 에러 수정할것
+			# TODO get_lat_AND_lon 함수 삭제되었음
 			ms_code = row.at['측정소코드']
 			lat_A_lon = get_lat_AND_lon(row.at['주소'])
 			result_excel.at[ms_code, 'lat'] = round(lat_A_lon[0], 2)
@@ -55,34 +65,29 @@ def get_processed_data(year, quat):
 		if pd.notnull(row.at['PM10']):
 			sum_pm += row.at['PM10']
 			count += 1
-			
+	
 	return result_excel
 
-def make_data(dust_year_data, year):
-	sido_sgg_df = pd.read_excel('./../../data/sido_sgg.xlsx')
-	
+
+def make_data(dust_year_data, sido_sgg_dict):
 	result_excel = pd.DataFrame(columns=['lat', 'lon'])
 	
-	for index in sido_sgg_df.index:
-		row = sido_sgg_df.loc[index]
-		lat_A_lon = get_lat_AND_lon(row.at['address'])
-		index_name = row.at['sido_code'] + '_' + row.at['sgg_code']
-		
-		result_excel.at[index_name, 'lat'] = round(2, lat_A_lon[0])
-		result_excel.at[index_name, 'lon'] = round(2, lat_A_lon[1])
+	for sido_sgg in sido_sgg_dict.keys():
+		result_excel.at[sido_sgg, 'lat'] = sido_sgg_dict[sido_sgg]['lat']
+		result_excel.at[sido_sgg, 'lon'] = sido_sgg_dict[sido_sgg]['lon']
 	
 	for date in dust_year_data.columns:
 		# processed_excel 에서 열 = 날짜 하지만 첫 두 열은 lat과 lon 이므로 제외
 		if date == 'lat' or date == 'lon':
 			continue
-			
+		
 		data_lat = dust_year_data['lat'].tolist()
 		data_lon = dust_year_data['lon'].tolist()
 		data_dust = dust_year_data[date].tolist()
 		
 		# 측정소에서 운영을 하지 않았던 날도 있으므로 운영을 하지 않은 측정소는 열외
 		for i, e in enumerate(data_dust):
-			if np.isnan(e):
+			if np.isnan(e) or pd.isnull(e):
 				data_dust.pop(i)
 				data_lat.pop(i)
 				data_lon.pop(i)
@@ -90,25 +95,28 @@ def make_data(dust_year_data, year):
 		func_result = idwr(x=data_lat, y=data_lon, z=data_dust, xi=result_excel['lat'].tolist(), yi=result_excel['lon'].tolist())
 		func_df = pd.DataFrame(func_result, columns=['lat', 'lon', date])  # 함수의 결과값이 이중리스트이므로 데이터프레임화 해서 date 열만 사용
 		result_excel[date] = func_df[date]
-	
-	os.makedirs('./../../data/dust', exist_ok=True)
-	result_excel.to_csv('./../../data/dust/' + year + 'dust.csv')
-	
+		
+		os.makedirs('../../data/dust', exist_ok=True)
+		result_excel.to_csv('../../data/dust/' + year + 'dust.csv')
+
+
 def main():
+	sido_sgg_dict = get_sido_sgg_code()
 	for y in range(start_year, end_year + 1):
 		year_data = None
 		year = str(y)
 		for q in range(1, 5):
 			quat = str(q)
-			dust_day_data = get_processed_data(year, quat)
+			quat_dust_day_data = get_processed_data(year, quat)
 			
 			if year_data is None:
-				year_data = pd.DataFrame(columns=quat_data.columns)
+				year_data = pd.DataFrame(columns=quat_dust_day_data.columns)
 				year_data.index.name = ms_code
 			
-			dust_data_quat_to_year(year_data, quat_data)
-			
-		make_data(year_data, year)
+			dust_data_quat_to_year(year_data, quat_dust_day_data)
+		
+		make_data(year_data, sido_sgg_dict)
+
 
 def input_dust_day_data(df, ms_code, date, sum_pm, count):
 	if count == 0 or ms_code is None:
@@ -116,12 +124,7 @@ def input_dust_day_data(df, ms_code, date, sum_pm, count):
 	else:
 		df.at[ms_code, date] = int(sum_pm / count)
 		return True
-	
-def get_lat_AND_lon(address):
-	geometry = map.geocode(address)[0]['geometry']['location']
-	lat = round(geometry['lat'], 2)
-	lon = round(geometry['lon'], 2)
-	return lat, lon
+
 
 def dust_data_quat_to_year(year_data, quat_data):
 	if len(year_data.index) != len(quat_data.index):
@@ -134,6 +137,7 @@ def dust_data_quat_to_year(year_data, quat_data):
 		else:
 			year_data[col] = quat_data[col]
 	return True
+
 
 # ------------------------------------------------------------
 # Distance calculation, degree to km (Haversine method)
@@ -166,3 +170,6 @@ def idwr(x, y, z, xi, yi):
 		xyzi = [xi[p], yi[p], u]
 		lstxyzi.append(xyzi)
 	return lstxyzi
+
+
+main()
